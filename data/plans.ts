2793d78fa119ef117fde886b8fe633d6dbec0797
MTS4YOU <129235350+MTS4YOU@users.noreply.github.com>
2025-10-29 21:22:28 +0700

    Update plans.ts

--- data/plans.ts
+++ data/plans.ts
@@ -5,7 +5,7 @@ export const plans = [
     memory: 1024,
     disk: 1120,
     cpu: 50,
-    price: 2000,
+    price: 2,
     description: "Paket hemat untuk keperluan ringan",
     features: ["1GB RAM", "1GB Disk", "50% CPU", "Support 24/7"],
   },
