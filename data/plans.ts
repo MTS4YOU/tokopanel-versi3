36cd053ac7ba87864492bc2080eddcedc8d13471
MTS4YOU <129235350+MTS4YOU@users.noreply.github.com>
2025-11-06 01:00:21 +0700

    Update plans.ts

--- data/plans.ts
+++ data/plans.ts
@@ -6,7 +6,7 @@ export const plans = [
     disk: 1025,
     cpu: 40,
     price: 2000,
-    description: "Paket hemat untuk keperluan ringan",
-    features: ["𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗦𝗲𝗿𝘃𝗲𝗿", "⏱️ Masa Aktif ±1 Bulan", "🛠️ Garansi 10 Hari"],
+    description: "Paket hemat untuk script bot ringan",
+    features: ["𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗦𝗲𝗿𝘃𝗲𝗿", "Support NodeJS V15-V24", "Masa Aktif ±1 Bulan", "Garansi 10 Hari", "Terlindungi Dari Maling Script"],
   },
   ]
