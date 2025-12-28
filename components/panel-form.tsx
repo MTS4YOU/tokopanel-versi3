6b923cc46ac305e4cb4ef223815da55c95d53ebe
MTS4YOU <129235350+MTS4YOU@users.noreply.github.com>
2025-12-28 21:55:25 +0700

    Update panel-form.tsx

--- components/panel-form.tsx
+++ components/panel-form.tsx
@@ -134,7 +134,7 @@ export default function PanelForm() {
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              placeholder="Tanpa Spasi!!"
+              placeholder="Masukan Tanpa Spasi!!"
               required
               className="h-14 text-base pl-10 bg-dark-500 border-dark-300 focus:border-red-500 focus:ring-red-500"
             />
@@ -155,7 +155,7 @@ export default function PanelForm() {
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              placeholder="Masukkan email penerima"
+              placeholder="Masukkan Email Penerima"
               required
               className="h-14 text-base pl-10 bg-dark-500 border-dark-300 focus:border-red-500 focus:ring-red-500"
             />
